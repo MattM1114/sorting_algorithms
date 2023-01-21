f4deda36596150cedb2f2aeccffce17c8ad62d02
MattM1114 <matt.magadza@gmail.com>
2023-01-21 12:26:50 -0800

    0 fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -30,12 +30,12 @@ void bubble_sort(int *array, size_t size)
 		return;
 
 	while (bubbly == false)
-        {
+	{
 		bubbly = true;
-        for (i = 0; i < len - 1; i++)
-        {
-        if (array[i] > array[i + 1])
-            {
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
